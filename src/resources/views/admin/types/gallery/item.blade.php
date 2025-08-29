518b2ac32f3a68b74dd716dbeaa5113d1add85b3
Labrock <vladimirpeshkov5@yandex.ru>
2025-08-29 17:22:55 +0300

    change admin appearance

--- src/resources/views/admin/types/gallery/item.blade.php
+++ src/resources/views/admin/types/gallery/item.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <div>{{ $item->title }}</div>
-    <div class="prose max-w-none">{!! $item->recordable->markdown !!}</div>
+    <h3 class="text-2xl font-medium mb-indent-half">{{ $item->title }}</h3>
+    <div class="prose max-w-[952px]">{!! $item->recordable->markdown !!}</div>
 </div>
